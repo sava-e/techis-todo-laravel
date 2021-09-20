868be505d93e53cd5adcb57d8327c7c8c84cbae1
sava-e <85050143+sava-e@users.noreply.github.com>
2021-09-20 19:49:08 +0900

    change provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        // if(\App::environment(['production'])){
-        //     \URL::forseScheme('https');
-        // }
+        
+        if(\App::environment(['production'])){
+            \URL::forseSchema('https');
+        }
     }
 }
