42b5a6abdf204f1cf57dea0344d2ed4bfde7b36f
sava-e <85050143+sava-e@users.noreply.github.com>
2021-09-21 21:56:27 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
-        if(\App::environment(['production'])){
+        // 
+        if(\App::environment(['production'])||\App::environment(['develop'])){
             \URL::forceScheme('https');
         }
     }
